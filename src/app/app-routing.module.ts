6929b42b975b2805c56100aa697c03c50beec05f
LauraEllen17 <laura.ellen@inovafarm.com.br>
2021-07-13 20:53:29 -0300

    routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DetailsComponent } from './details/details.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', redirectTo: '/initial', pathMatch: 'full' },
+    { path: 'heroes', component: HeroesComponent },
+    { path: 'details', component: DetailsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { InitialComponent } from './initial/initial.component';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    InitialComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
