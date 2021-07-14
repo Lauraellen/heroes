33c0f251ddd476901b9acf0cf5bd8526784f8cfd
LauraEllen17 <laura.ellen@inovafarm.com.br>
2021-07-14 14:13:59 -0300

    background-image

--- src/app/initial/initial.component.ts
+++ src/app/initial/initial.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-initial',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InitialComponent implements OnInit {
 
-  constructor() { }
+  tutorials: any;
+  currentTutorial = null;
+  currentIndex = -1;
+  title = '';
+
+  page = 1;
+  count = 0;
+  pageSize = 3;
+  pageSizes = [3, 6, 9];
+
+  constructor(
+    @Inject(HeroService) private heroService,
+    private router: Router,
+  ) { 
+    
+  }
 
   ngOnInit(): void {
+    /*this.heroService.getHeroes().subscribe((data) => {
+      console.log(data);
+    });*/
   }
+
+
+  choiceHeroes(): void {
+    console.log('ta funcionando')
+    this.router.navigate(['/ngx-datatable']);
+  }
+  
 
 }
